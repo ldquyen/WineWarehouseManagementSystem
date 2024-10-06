b806b4e7dff89d830baaa0b82cfc4fcf58fe6a53
Quyền Lê <daiquyen0@gmail.com>
2024-10-06 13:25:57 +0700

    udpate all DAO

--- DataAccessLayer/ImportDAO.cs
+++ DataAccessLayer/ImportDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    internal class ImportDAO
+    public class ImportDAO : SingletonBase<ImportDAO>
     {
+        public async Task AddImport(Import import)
+        {
+            await _context.Imports.AddAsync(import);
+            await _context.SaveChangesAsync();
+        }
     }
 }
